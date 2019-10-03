1f8c812bed4c761b7231a4aa2984e043249ddc09
Mukul Kumar <samratmukulkumar@gmail.com>
2019-10-03 13:04:11 +0530

    #

--- src/java/Servlets/LoginCheck.java
+++ src/java/Servlets/LoginCheck.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
- @MultipartConfig
 public class LoginCheck extends HttpServlet {
 
     protected void service(HttpServletRequest request, HttpServletResponse response)throws ServletException,IOException
