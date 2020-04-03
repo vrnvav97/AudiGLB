408215d8230c9d722b636a880e0c2562ff62dd4a
Varun Agrawal <vrnvav97@rediffmail.com>
2020-04-03 23:10:05 +0530

    #

--- build/web/js/confirmFormjs.js
+++
@@ -1,9 +0,0 @@
-var a = document.querySelector(".cancel");
-          a.addEventListener('click',()=>{
-              var result = confirm('Do you really want to Cancel Booking');
-              if (result===true)
-              {
-                  document.write("Your booking request has been cancelled !");
-//                  window.navigate(http://localhost:27016/AudiGLB/userDashboard.jsp);
-              }
-          });
